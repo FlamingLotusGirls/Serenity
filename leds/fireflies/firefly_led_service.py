13508edfe45a75a62fbfbcdbc0eddfdb045a5589
Carolyn Wales <cswales@gmail.com>
2019-07-18 08:51:09 -0700

    led service that actually works

--- leds/fireflies/firefly_led_service.py
+++ leds/fireflies/firefly_led_service.py
@@ -9,7 +9,7 @@ import requests
 import urllib.request, urllib.parse, urllib.error
 from threading import Thread
 
-import led_controller
+from led_controller import FireflyLedController
 
 ''' 
     REST service for controlling firefly leds
@@ -22,16 +22,50 @@ logger = logging.getLogger("firefly_leds")
 app = Flask("flg", static_url_path="", static_folder="/home/flaming/Serenity/leds/fireflies/static")
 #app = Flask("flg", static_url_path="")
 
+
 def serve_forever(httpPort=PORT):
     logger.info("Fireflies LED WebServer: port {}".format(httpPort))
     app.run(host="0.0.0.0", port=httpPort, threaded=True) ## XXX - FIXME - got a broken pipe on the socket that terminated the application (uncaught exception) supposedly this is fixed in flask 0.12
 
-g_led_state = [ 
-  {'swarm':0, 'red':255, 'green':255, 'blue':255, 'speed':1, 'sequence':'0000011111'},
-  {'swarm':1, 'red':255, 'green':255, 'blue':255, 'speed':1, 'sequence':'0000011111'},
-  {'swarm':2, 'red':255, 'green':255, 'blue':255, 'speed':1, 'sequence':'0000011111'},
-  {'swarm':3, 'red':255, 'green':255, 'blue':255, 'speed':1, 'sequence':'0000011111'},
-]
+
+class CORSResponse(Response):
+    def __init__(self, status_str, status_code, mimetype="text/plain"):
+        super().__init__(status_str, status_code, mimetype=mimetype)
+        self.headers['Access-Control-Allow-Origin'] = '*'
+
+
+class JSONResponse(CORSResponse):
+    def __init__(self, status_str, status_code=200):
+        super().__init__(status_str, status_code, "application/json")
+
+
+def parse_colors(colors):
+    colorarray = colors.split(",")
+    if len(colorarray) != 3:
+        raise ValueError
+    colorvals = []
+    for color in colorarray:
+        colorval = int(color)
+        if colorval < 0 or colorval > 255:
+            raise ValueError
+        colorvals.append(int(colorval))
+        
+    return colorvals[0], colorvals[1], colorvals[2]
+
+
+def parse_speed(speed):
+    speedval = int(speed)
+    if speedval < 1 or speedval > 10:
+        raise ValueError
+    return speedval
+
+
+def parse_sequence(sequence):
+    for character in sequence:
+        if character != '0' and character != '1':
+            raise ValueError
+    return sequence
+    
 
 @app.route("/firefly_leds", methods=['GET', 'POST'])
 def firefly_status():
@@ -44,58 +78,70 @@ def firefly_status():
     '''
     if request.method == 'POST':
         if not 'swarm' in request.values:
-            return Response("Must have 'swarm' value", 400)
+            return CORSResponse("Must have 'swarm' value", 400)
         try:
             swarm_id = int(request.values['swarm'])
             if swarm_id < 0 or swarm_id > 3:
                 raise Exception
-            current_leds = led_controller.get_led_patterns()
+            current_leds = controller.get_led_patterns()
+            for leds in current_leds:
+                if leds['board_id'] == swarm_id:
+                    led_settings = leds
+                    break
         except:
-            return Response("Invalid swarm value", 400)
+            return CORSResponse("Invalid swarm value", 400)
 
         if 'color' in request.values:
             try:
-                red, green, blue = parse_color(request.values['color'])
+                red, green, blue = parse_colors(request.values['color'])
             except:
-                return Response("Invalid color values", 400)
+                return CORSResponse("Invalid color values", 400)
+            if red < 0 \
+                or red > 255 \
+                or green < 0 \
+                or green > 255 \
+                or blue < 0 \
+                or blue > 255:
+                return CORSResponse("Invalid color values", 400)
+            red = red/255
+            green = green/255
+            blue = blue/255  
         else:
-            red = current_leds['red']
-            green = current_leds['green']
-            blue = current_leds['blue'] 
+            red = led_settings['color'][0]
+            green = led_settings['color'][1]
+            blue = led_settings['color'][2] 
     
         if 'speed' in request.values:
             try:
                 speed = parse_speed(request.values['speed'])
             except:
-                return Response("Invalid speed values", 400) 
+                return CORSResponse("Invalid speed values", 400) 
         else:
-            speed = current_leds['speed']
+            speed = led_settings['speed']
     
         if 'sequence' in request.values:
             try:
                 sequence = parse_sequence(request.values['sequence'])
             except:
-                return Response("Invalid sequence values", 400)
+                return CORSResponse("Invalid sequence values", 400)
         else:
-            sequence = current_leds['sequence']
+            sequence = led_settings['pattern']
             
-        led_controller.send_pattern(swarm_id, red, green, blue, speed, sequence)
+        controller.set_led_pattern(swarm_id, red, green, blue, speed, sequence)
 
-        return Response("", 200)
+        return CORSResponse("", 200)
 
     else:
-        return makeJsonResponse(json.dumps(led_controller.get_led_patterns()))
+        foo = controller.get_led_patterns()
+        return JSONResponse(json.dumps(controller.get_led_patterns()))
 
-def makeJsonResponse(jsonString, VrespStatus=200):
-    resp = Response(jsonString, status=respStatus, mimetype='application/json')
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
 
 
 if __name__ == "__main__":
 
     logging.basicConfig(format='%(asctime)-15s %(levelname)s %(module)s %(lineno)d: %(message)s', level=logging.DEBUG)
 
+    controller = FireflyLedController() 
     flaskThread = Thread(target=serve_forever, args=[7000])
     flaskThread.start()
 
--- leds/fireflies/led_controller.py
+++ leds/fireflies/led_controller.py
@@ -1,20 +1,92 @@
 import socket
 import time
 import sys
+from threading import Thread
+from threading import Lock
 
-ttl = 8
-def createBroadcastSender(ttl=4):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
-
-    return sock
-
-multicast_group = '224.3.29.71'
+NUM_SWARMS = 4
+MULTICAST_GROUP = '224.3.29.71'
 #multicast_group = '224.0.0.1'
 #BROADCAST_ADDR = "255.255.255.255"
 LED_CMD_PORT   = 5010
+
+class FireflyLedController():
+    class FireflyPattern():
+        def __init__(self, board_id, red, green, blue, speed, pattern_str):
+            self.board_id = board_id
+            self.red = red
+            self.blue = blue
+            self.green = green
+            self.speed = speed
+            self.pattern_str = pattern_str
+            self.packet = create_firefly_packet(
+                self.board_id,
+                [red, green, blue],
+                self.speed,
+                self.pattern_str
+            )
+
+        def get_minimal(self):
+            return {
+                'board_id':self.board_id,
+                'color': [self.red, self.green, self.blue],
+                'speed': self.speed,
+                'pattern': self.pattern_str
+            }       
+    def __init__(self):
+        self.sender_socket = self.createBroadcastSender()
+        self.patterns = {
+            board_id : FireflyLedController.FireflyPattern(board_id, 0.5, 0.7, 0.5, 2, "01100110000") \
+            for board_id in range(NUM_SWARMS)}
+
+        self.patternLock = Lock()
+        self.is_running = True
+        self.broadcastThread = Thread(target=self.broadcastFireflyPatterns)
+        self.broadcastThread.start()
+
+
+    def set_led_pattern(self, board_id, red, green, blue, speed, pattern_str):
+        self.patternLock.acquire()
+        self.patterns[board_id] = FireflyLedController.FireflyPattern(
+            board_id,
+            red,
+            green,
+            blue,
+            speed,
+            pattern_str
+        )
+        self.patternLock.release()
+
+
+    def get_led_patterns(self):
+        self.patternLock.acquire()
+        pattern_copy = [pattern.get_minimal() for pattern in set(self.patterns.values())]
+        self.patternLock.release()
+        return pattern_copy
+
+
+    def broadcastFireflyPatterns(self):
+        while self.is_running:
+            self.patternLock.acquire()
+            try:
+                for pattern in self.patterns:
+                    self.sender_socket.sendto(packet, (MULTICAST_GROUP, LED_CMD_PORT))
+                    time.sleep(0.01)
+            except Exception:
+                pass # I'm concerned about transient networking issues...
+                     # and I really don't know what to do if they happen
+            self.patternLock.release()
+            time.sleep(1.0)
+                
+    def createBroadcastSender(self, ttl=4):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#       self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
+
+
+
+
 
 def simple_checksum(data):
     print(f"Data len is {len(data)}")
