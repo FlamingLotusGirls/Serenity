0af5f886069a250c419c5d3d9759624ed9291391
Brian Bulkowski <brian@bulkowski.org>
2019-08-07 02:12:06 -0700

    Fixed typos, seems to run now

--- sound/SoundButtons/sound_button_service.py
+++ sound/SoundButtons/sound_button_service.py
@@ -6,34 +6,40 @@ SOUND_SERVER = "localhost:8000"
 
 buttons = [
     # Bank 1
-    {id:1, type='cricket', function='on', button=Button("BOARD11", bounce_time=0.1)},
-    {id:2, type='cricket', function='off', button=Button("BOARD12", bounce_time=0.1)},
-    {id:3, type='cicada', function='on', button=Button("BOARD13", bounce_time=0.1)},
-    {id:4, type='cicada', function='off', button=Button("BOARD15", bounce_time=0.1)},
-    {id:5, type='katydid', function='on', button=Button("BOARD16", bounce_time=0.1)},
-    {id:6, type='kaytdid', function='off', button=Button("BOARD18", bounce_time=0.1)},
+    {'id':1, 'type': 'cricket', 'function':'on', 'button': Button("BOARD11", bounce_time=0.1)},
+    {'id':2, 'type': 'cricket', 'function':'off', 'button': Button("BOARD12", bounce_time=0.1)},
+    {'id':3, 'type': 'cicada', 'function': 'on', 'button': Button("BOARD13", bounce_time=0.1)},
+    {'id':4, 'type': 'cicada', 'function': 'off', 'button': Button("BOARD15", bounce_time=0.1)},
+    {'id':5, 'type': 'katydid', 'function': 'on', 'button': Button("BOARD16", bounce_time=0.1)},
+    {'id':6, 'type': 'kaytdid', 'function': 'off', 'button': Button("BOARD18", bounce_time=0.1)},
     # Bank 2
-    {id:7, type='peeper', function='on', button=Button("BOARD33", bounce_time=0.1)},
-    {id:8, type='peeper', function='off', button=Button("BOARD35", bounce_time=0.1)},
-    {id:9, type='whippoorwill', function='on', button=Button("BOARD36", bounce_time=0.1)},
-    {id:10, type='whippoorwill', function='off', button=Button("BOARD37", bounce_time=0.1)},
-    {id:11, type='owl', function='on', button=Button("BOARD38", bounce_time=0.1)},
-    {id:12, type='owl', function='off', button=Button("BOARD40", bounce_time=0.1)},
+    {'id':7, 'type': 'peeper', 'function': 'on', 'button': Button("BOARD33", bounce_time=0.1)},
+    {'id':8, 'type': 'peeper', 'function': 'off', 'button': Button("BOARD35", bounce_time=0.1)},
+    {'id':9, 'type': 'whippoorwill', 'function': 'on', 'button': Button("BOARD36", bounce_time=0.1)},
+    {'id':10, 'type': 'whippoorwill', 'function': 'off', 'button': Button("BOARD37", bounce_time=0.1)},
+    {'id':11, 'type': 'owl', 'function': 'on', 'button': Button("BOARD38", bounce_time=0.1)},
+    {'id':12, 'type': 'owl', 'function': 'off', 'button': Button("BOARD40", bounce_time=0.1)},
 ]
+
+# board_button is the object that triggered
 
 def send_sound_action(board_button):
     
     r = requests.post(
-        "http://" + SOUND_SERVER + "/effects/" + button['type'],
-        data={"onOff:" button['function']}
+        "http://" + SOUND_SERVER + "/effects/" + board_button['type'],
+        data={"onOff": board_button['function']}
     )
     print(r.status_code, r.reason)
    
 def init():
-    for button in buttons:
-        button.when_pressed = send_sound_action
+    for b in buttons:
+        b['button'].when_pressed = send_sound_action
  
 def main():
     while(True):
         sleep(0.1) # is there every any reason to wake up?
+
+if __name__ == "__main__":
+    init()
+    main()
 
