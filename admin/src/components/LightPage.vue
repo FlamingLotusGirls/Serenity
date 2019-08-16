fc6791b21cecf7b8776e710d202cb38f9e79ef23
Charlie Stigler <charlie@charliestigler.com>
2019-08-16 12:15:35 -0700

    Remove UI for Swarm Four since we only have three now

--- admin/src/components/LightPage.vue
+++ admin/src/components/LightPage.vue
@@ -26,10 +26,9 @@
                 </div>
             </div>
             <div class="small-bugs-content row">
-                <SwarmLightingWidget v-bind:swarmNumber="0" swarmNumberString="One" class="col-3" />
-                <SwarmLightingWidget v-bind:swarmNumber="1" swarmNumberString="Two" class="col-3" />
-                <SwarmLightingWidget v-bind:swarmNumber="2" swarmNumberString="Three" class="col-3" />
-                <SwarmLightingWidget v-bind:swarmNumber="3" swarmNumberString="Four" class="col-3" />
+                <SwarmLightingWidget v-bind:swarmNumber="0" swarmNumberString="One" class="col-4" />
+                <SwarmLightingWidget v-bind:swarmNumber="1" swarmNumberString="Two" class="col-4" />
+                <SwarmLightingWidget v-bind:swarmNumber="2" swarmNumberString="Three" class="col-4" />
             </div>
         </div>
         <!-- these APIs got cut so do not exist!
