9c2ab812f9823c9ad6631108f18e6a695d893f80
Charlie Stigler <charlie@charliestigler.com>
2019-08-17 22:34:44 -0600

    Jar LED controls/requests mostly working

--- admin/src/appConfig.js
+++ admin/src/appConfig.js
@@ -1,10 +1,12 @@
 // define network paths to various controllers
 const fireControllerURL = 'http://localhost:5000';
 const smallFireflyLEDControllerURL = 'http://localhost:7000';
+const jarLEDControllerURL = 'http://localhost:8000';
 const soundControllerURL = 'http://localhost:9000';
 
 export {
     fireControllerURL,
     smallFireflyLEDControllerURL,
+    jarLEDControllerURL,
     soundControllerURL
 };
--- admin/src/components/JarLightingWidget.vue
+++ admin/src/components/JarLightingWidget.vue
@@ -2,28 +2,65 @@
     <div class="jar-lighting-widget">
         <!-- inner div is just a hack so our borders can go inside of the Bootstrap grid gutters -->
         <div class="jar-lighting-widget-inner">
-            <label>Set {{bugName}}'s Color Palette</label>
-            <ColorPicker class="color-slider" v-model="selectedColor" />
-            <label>Select a Jar Pattern for {{bugName}}</label>
-            <select class="custom-select">
-                <option value="0" selected>soothingFlow</option>
+            <label>Foreground jar pattern for {{bugName}}</label>
+            <select class="custom-select" v-model="selectedForegroundPattern" v-on:change="saveJarSettings">
+                <option v-for="patternName in foregroundPatternNames" v-bind:value="patternName">{{patternName}}</option>
             </select>
+            <label>Background jar pattern for {{bugName}}</label>
+            <select class="custom-select" v-model="selectedBackgroundPattern" v-on:change="saveJarSettings">
+                <option v-for="patternName in backgroundPatternNames" v-bind:value="patternName">{{patternName}}</option>
+            </select>
+            <label for="jarIntensity">Set {{bugName}}'s Jar LED Intensity:</label>
+            <input type="range" name="jarIntensity" class="jar-intensity-control" id="jarIntensity" min="0" max="1" step="0.01" v-model="intensity" v-on:change="saveJarSettings" />
         </div>
     </div>
 </template>
 
 <script>
 import ColorPicker from './ColorPicker';
+import { getJarLEDPatternLists, getJarLEDs, setJarLEDs } from '../requests';
 
 export default {
     props: ['bugName'],
+    beforeMount() {
+        // get lists of jar LED patterns to show in the menus
+        getJarLEDPatternLists()
+            .then(lists => {
+                this.backgroundPatternNames = lists.backgrounds.map(obj => obj.name);
+                this.foregroundPatternNames = lists.foregrounds;
+                console.log('background: ', this.foregroundPatternNames, 'foreground', this.foregroundPatternNames);
+            }, error => {
+                alert(error);
+            });
+
+        // fetch current jar LED settings
+        getJarLEDs()
+            .then((settings) => {
+                this.selectedForegroundPattern = settings.foreground;
+                this.selectedBackgroundPattern = settings.background;
+                this.intensity = settings.intensity;
+            }, error => {
+                alert(error);
+            });
+    },
     data () {
         return {
-            selectedColor: '#ff0000'
+            backgroundPatternNames: [],
+            foregroundPatternNames: [],
+            selectedForegroundPattern: null,
+            selectedBackgroundPattern: null,
+            intensity: 1
         };
     },
-    components: {
-        ColorPicker
+    methods: {
+        saveJarSettings() {
+            return setJarLEDs(this.selectedForegroundPattern, this.selectedBackgroundPattern, this.intensity)
+                .then(() => {
+                    console.log(`Saved jar LED settings`);
+                }, error => {
+                    alert(error);
+                });
+        }
     }
 };
 </script>
@@ -36,5 +73,8 @@ export default {
 }
 .jar-lighting-widget .custom-select {
     margin-bottom: 12px;
+}
+.jar-intensity-control {
+    width: 100%;
 }
 </style>
