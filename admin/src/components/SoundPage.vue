533630238996a9baa74f90d4fb19ddbf3929eafb
Charlie Stigler <charlie@charliestigler.com>
2019-08-17 23:55:50 -0600

    WIP leading towards sound requests working

--- admin/src/components/SoundPage.vue
+++ admin/src/components/SoundPage.vue
@@ -1,15 +1,8 @@
 <template>
     <div class="sound-page container">
-        <div class="top-section row">
-            <div class="master-volume-control col-3">
-                <h3>🔊</h3>
-                <div class="volume-control-internal">
-                    <label for="masterVolume">Set Master Volume:</label>
-                    <input type="range" name="masterVolume" id="masterVolume" />
-                </div>
-            </div>
-            <div class="soundscape-selector col-3">
-                <label for="soundscapeName">Select a Soundscape:</label>
+        <div class="soundscape-row">
+            <div class="soundscape-selector col-12">
+                <label for="soundscapeName">Choose a Soundscape to Play:</label>
                 <select class="custom-select" name="soundscapeName" id="soundscapeName">
                     <option value="0" selected>Texas Hill Country</option>
                     <option value="1">Charlie's Cool Soundscape</option>
@@ -17,42 +10,83 @@
                     <option value="3">Burn Baby Burn</option>
                 </select>
             </div>
-            <div class="balance-controls col-4">
-                <label>Set Balance Controls:</label>
-                <BalanceControl controllerId="leftpergolanear">Left Pergola Near</BalanceControl>
-                <BalanceControl controllerId="leftpergolamid">Left Pergola Mid</BalanceControl>
-                <BalanceControl controllerId="leftpergolafar">Left Pergola Far</BalanceControl>
-                <BalanceControl controllerId="rightpergolanear">Right Pergola Near</BalanceControl>
-                <BalanceControl controllerId="rightpergolamid">Right Pergola Mid</BalanceControl>
-                <BalanceControl controllerId="rightpergolafar">Right Pergola Far</BalanceControl>
-                <BalanceControl controllerId="metric">Metric</BalanceControl>
-                <BalanceControl controllerId="brazen">Brazen</BalanceControl>
-                <BalanceControl controllerId="john">John</BalanceControl>
-                <BalanceControl controllerId="jar3">Jar 3</BalanceControl>
-                <BalanceControl controllerId="fueldepot">Fuel Depot</BalanceControl>
-            </div>
-            <button class="toggle-mute-button col-2 btn" v-on:click="toggleMute">
-                <span class="unmute-icon">{{ muted ? '🔊' : '🔇' }}</span>
-                <br>
-                {{ muted ? 'Unmute' : 'Mute' }} All Sound
-            </button>
         </div>
-        <div class="bottom-section row">
-            <p>Tap a button to add that sound. Double-tap to add more of that sound. Tap a third time to turn off. Max 6.</p>
-            <div class="single-sound-buttons">
-                <SoundSnippetButton>Wind</SoundSnippetButton>
-                <SoundSnippetButton>Owl</SoundSnippetButton>
-                <SoundSnippetButton>Leaves Rustling</SoundSnippetButton>
-                <SoundSnippetButton>Wind 2</SoundSnippetButton>
-                <SoundSnippetButton>Dry Grasses</SoundSnippetButton>
-                <SoundSnippetButton>Distant Thunder</SoundSnippetButton>
-                <SoundSnippetButton>Songbird</SoundSnippetButton>
-                <SoundSnippetButton>Frog</SoundSnippetButton>
-                <SoundSnippetButton>Cricket</SoundSnippetButton>
-                <SoundSnippetButton>Cicada</SoundSnippetButton>
-                <SoundSnippetButton>Sparrow Chirp</SoundSnippetButton>
-                <SoundSnippetButton>Raven</SoundSnippetButton>
+        <hr>
+        <div class="main-section row">
+            <div class="left-column col-4">
+                <div class="master-volume-control">
+                    <label for="masterVolume">Set Master Volume:</label>
+                    <input type="range" name="masterVolume" id="masterVolume" />
+                </div>
+                <div class="zone-volume-controls">
+                    <label>Set Zone Volume Controls:</label>
+                    <BalanceControl controllerId="pergolaleft">Pergola Left</BalanceControl>
+                    <BalanceControl controllerId="pergolaright">Pergola Right</BalanceControl>
+                    <BalanceControl controllerId="jars">Jars</BalanceControl>
+                    <BalanceControl controllerId="fueldepot">Fuel Depot</BalanceControl>
+                </div>
             </div>
+            <div class="right-columns col-8">
+                <div class="ambient-tracks row">
+                    <div class="col-6">
+                    </div>
+                    <div class="col-6">
+                    </div>
+                </div>
+                <p>
+                    Tap a button to activate a sound. Double tap to play moderate density of sound.  Triple tap to get maximum density of sound.  Volume can be set for each sound.
+                </p>
+                <p>
+                    When activated, tap once to turn off. 
+                </p>
+                <label>Set individual sound densities and volumes</label>
+                <div class="single-sound-buttons">
+                    <div class="individual-sound-row row">
+                        <div class="col-6 single-sound">
+                            <SoundSnippetButton>Wind</SoundSnippetButton>
+                            <input type="range" />
+                        </div>
+                        <div class="col-6 single-sound">
+                            <SoundSnippetButton>Owl</SoundSnippetButton>
+                            <input type="range" />
+                        </div>
+                    </div>
+                    <div class="individual-sound-row row">
+                        <div class="col-6 single-sound">
+                            <SoundSnippetButton>Leaves Rustling</SoundSnippetButton>
+                            <input type="range" />
+                        </div>
+                        <div class="col-6 single-sound">
+                            <SoundSnippetButton>Wind 2</SoundSnippetButton>
+                            <input type="range" />
+                        </div>
+                    </div>
+                    <div class="individual-sound-row row">
+                        <div class="col-6 single-sound">
+                            <SoundSnippetButton>Dry Grasses</SoundSnippetButton>
+                            <input type="range" />
+                        </div>
+                        <div class="col-6 single-sound">
+                            <SoundSnippetButton>Distant Thunder</SoundSnippetButton>
+                            <input type="range" />
+                        </div>
+                    </div>
+                    <div class="individual-sound-row row">
+                        <div class="col-6 single-sound">
+                            <SoundSnippetButton>Songbird</SoundSnippetButton>
+                            <input type="range" />
+                        </div>
+                        <div class="col-6 single-sound">
+                            <SoundSnippetButton>Frog</SoundSnippetButton>
+                            <input type="range" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="save-soundscape-section row">
+            <input type="text" placeholder="Enter a unique name">
+            <button class="btn btn-primary" type="submit">Save This Soundscape</button>
         </div>
     </div>
 </template>
@@ -64,7 +98,7 @@ import SoundSnippetButton from './SoundSnippetButton';
 export default {
     data: function() {
         return {
-            muted: false
+            
         };
     },
     components: {
@@ -72,37 +106,16 @@ export default {
         SoundSnippetButton
     },
     methods: {
-        toggleMute: function() {
-            this.muted = !this.muted;
-        }
     }
 };
 </script>
 
 <style>
-.master-volume-control > * {
-    display: inline-block;
+.soundscape-selector label {
+    margin-right: 16px;
 }
-.master-volume-control h3 {
-    font-size: 48px;
-    margin-right: 14px;
-}
-.master-volume-control .volume-control-internal > * {
-    display: block;
-}
-.toggle-mute-button {
-    font-size: 17px;
-    white-space: normal;
-    height: 136px;
-    vertical-align: middle;
-    background: #eee;
-    border: 1px solid #ccc
-}
-.toggle-mute-button:hover {
-    border: 1px solid #777;
-}
-.toggle-mute-button .unmute-icon {
-    font-size: 64px;
+.soundscape-selector select {
+    width: auto;
 }
 .bottom-section {
     margin: 40px 0;
