679efa6efa44ef6f315a7d3997b7c32af44d5bb1
Carolyn Wales <cswales@gmail.com>
2019-08-18 22:52:11 -0700

    Working button service

--- sound/SoundButtons/sound_button_service.py
+++ sound/SoundButtons/sound_button_service.py
@@ -5,30 +5,29 @@ from time import sleep
 SOUND_SERVER = "localhost:9000"
 
 buttons = [
-    # Bank 1
-    {'id':1, 'type': 'cricket', 'function':'on', 'button': Button("BOARD11", bounce_time=0.1)},
-    {'id':2, 'type': 'cricket', 'function':'off', 'button': Button("BOARD12", bounce_time=0.1)},
-    {'id':3, 'type': 'cicada', 'function': 'on', 'button': Button("BOARD13", bounce_time=0.1)},
-    {'id':4, 'type': 'cicada', 'function': 'off', 'button': Button("BOARD15", bounce_time=0.1)},
-    {'id':5, 'type': 'katydid', 'function': 'on', 'button': Button("BOARD16", bounce_time=0.1)},
-    {'id':6, 'type': 'kaytdid', 'function': 'off', 'button': Button("BOARD18", bounce_time=0.1)},
-    # Bank 2
-    {'id':7, 'type': 'peeper', 'function': 'on', 'button': Button("BOARD33", bounce_time=0.1)},
-    {'id':8, 'type': 'peeper', 'function': 'off', 'button': Button("BOARD35", bounce_time=0.1)},
-    {'id':9, 'type': 'whippoorwill', 'function': 'on', 'button': Button("BOARD36", bounce_time=0.1)},
-    {'id':10, 'type': 'whippoorwill', 'function': 'off', 'button': Button("BOARD37", bounce_time=0.1)},
-    {'id':11, 'type': 'owl', 'function': 'on', 'button': Button("BOARD38", bounce_time=0.1)},
-    {'id':12, 'type': 'owl', 'function': 'off', 'button': Button("BOARD40", bounce_time=0.1)},
+    {'id':7, 'group': 1, 'function': 1, 'button': Button("BOARD33", bounce_time=0.2)},
+    {'id':8, 'group': 1, 'function': 0, 'button': Button("BOARD35", bounce_time=0.2)},
+    {'id':9, 'group': 2, 'function': 1, 'button': Button("BOARD36", bounce_time=0.2)},
+    {'id':10, 'group': 2, 'function': 0, 'button': Button("BOARD37", bounce_time=0.2)},
+    {'id':11, 'group': 3, 'function': 1, 'button': Button("BOARD38", bounce_time=0.2)},
+    {'id':12, 'group': 3, 'function': 0, 'button': Button("BOARD40", bounce_time=0.2)},
 ]
 
 # board_button is the object that triggered
 
 def send_sound_action(board_button):
-    
-    r = requests.post(
-        "http://" + SOUND_SERVER + "/effects/" + board_button['type'],
-        data={"onOff": board_button['function']}
-    )
+    button = [button for button in buttons if button['button'] == board_button][0]
+    url = "http://" + SOUND_SERVER + "/audio/buttons/" + \
+          str(button['group']) + "/" + str(button['function'])
+    print(f"url is {url}")
+
+
+    r = requests.put(url)
+ 
+#	r = requests.put(
+#        "http://" + SOUND_SERVER + "/audio/buttons/" +
+#        board['group'] + "/" + board['function']
+#    )
     print(r.status_code, r.reason)
    
 def init():
