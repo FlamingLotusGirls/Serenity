c10431a15b8d55c74632707ffdb23ecd33303120
CSW <cswales@gmail.com>
2019-08-18 23:43:12 -0700

    working button service

--- sound/SoundButtons/sound_button_service.py
+++ sound/SoundButtons/sound_button_service.py
@@ -5,24 +5,29 @@ from time import sleep
 SOUND_SERVER = "localhost:9000"
 
 buttons = [
-    {'id':7, 'group': 1, 'function': 1, 'button': Button("BOARD33", bounce_time=0.1)},
-    {'id':8, 'group': 1, 'function': 0, 'button': Button("BOARD35", bounce_time=0.1)},
-    {'id':9, 'group': 2, 'function': 1, 'button': Button("BOARD36", bounce_time=0.1)},
-    {'id':10, 'group': 2, 'function': 0, 'button': Button("BOARD37", bounce_time=0.1)},
-    {'id':11, 'group': 3, 'function': 1, 'button': Button("BOARD38", bounce_time=0.1)},
-    {'id':12, 'group': 3, 'function': 0, 'button': Button("BOARD40", bounce_time=0.1)},
+    {'id':7, 'group': 1, 'function': 1, 'button': Button("BOARD33", bounce_time=0.2)},
+    {'id':8, 'group': 1, 'function': 0, 'button': Button("BOARD35", bounce_time=0.2)},
+    {'id':9, 'group': 2, 'function': 1, 'button': Button("BOARD36", bounce_time=0.2)},
+    {'id':10, 'group': 2, 'function': 0, 'button': Button("BOARD37", bounce_time=0.2)},
+    {'id':11, 'group': 3, 'function': 1, 'button': Button("BOARD38", bounce_time=0.2)},
+    {'id':12, 'group': 3, 'function': 0, 'button': Button("BOARD40", bounce_time=0.2)},
 ]
 
 # board_button is the object that triggered
 
 def send_sound_action(board_button):
-    button = [button in buttons if button['button'] == board_button]
-    print(f"button is {button}")
-    print(f"board_button is {board_button}")
-    r = requests.put(
-        "http://" + SOUND_SERVER + "/audio/buttons/" +
-        board_button['group'] + "/" + board_button['function']
-    )
+    button = [button for button in buttons if button['button'] == board_button][0]
+    url = "http://" + SOUND_SERVER + "/audio/buttons/" + \
+          str(button['group']) + "/" + str(button['function'])
+    print(f"url is {url}")
+
+
+    r = requests.put(url)
+ 
+#	r = requests.put(
+#        "http://" + SOUND_SERVER + "/audio/buttons/" +
+#        board['group'] + "/" + board['function']
+#    )
     print(r.status_code, r.reason)
    
 def init():
