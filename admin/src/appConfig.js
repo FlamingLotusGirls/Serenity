12ba4fb64cfbc48ff1a37958fc7989561feba7ba
Charlie Stigler <charlie@charliestigler.com>
2019-08-20 11:23:44 -0700

    add one-var change for services from debug/localhost to prod

--- admin/src/appConfig.js
+++ admin/src/appConfig.js
@@ -1,8 +1,24 @@
 // define network paths to various controllers
-const fireControllerURL = 'http://localhost:5000';
-const smallFireflyLEDControllerURL = 'http://localhost:7000';
-const jarLEDControllerURLs = ['http://localhost:8000', 'http://localhost:8000', 'http://localhost:8000'];
-const soundControllerURL = 'http://localhost:9000';
+const IS_PRODUCTION = true;
+
+const MASTER_PI_IP = '192.168.1.10';
+const JAR_1_IP = '192.168.1.11';
+const JAR_2_IP = '192.168.1.12';
+const JAR_3_IP = '192.168.1.13';
+
+let fireControllerURL, smallFireflyLEDControllerURL, jarLEDControllerURLs, soundControllerURL;
+
+if (IS_PRODUCTION) {
+    fireControllerURL = `http://${MASTER_PI_IP}:5000`;
+    smallFireflyLEDControllerURL = `http://${MASTER_PI_IP}:7000`;
+    jarLEDControllerURLs = [`http://${JAR_1_IP}:8000`, `http://${JAR_2_IP}:8000`, `http://${JAR_3_IP}:8000`];
+    soundControllerURL = `http://${MASTER_PI_IP}:9000`;
+} else {
+    fireControllerURL = `http://localhost:5000`;
+    smallFireflyLEDControllerURL = `http://localhost:7000`;
+    jarLEDControllerURLs = [`http://localhost:8000`, `http://localhost:8000`, `http://localhost:8000`];
+    soundControllerURL = `http://localhost:9000`;
+}
 
 export {
     fireControllerURL,
