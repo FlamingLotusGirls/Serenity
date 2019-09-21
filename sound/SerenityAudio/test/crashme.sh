a8faf5282219df837845e5b5b52b6c198a762a0e
Brian Bulkowski <brian@bulkowski.org>
2019-09-21 13:28:02 -0700

    Tests directly to SerenityAudio that show the reference counting issues

---
+++ sound/SerenityAudio/test/crashme.sh
@@ -0,0 +1,14 @@
+echo "set frotgs 1entensity 0"
+curl -s -d '{"effects":{"crickets":{"intensity":0,"volume":50},"birds":{"intensity":2,"volume":45},"cicadas":{"intensity":0,"volume":0},"frogs":{"intensity":0,"volume":50},"frogsAlt":{"intensity":0,"volume":0}}}' -H 'Content-Type: application/json' -X PUT http://192.168.1.8:8000/soundscape > /dev/null
+sleep 1
+echo "set frogs 1entensity 1"
+curl -s -d '{"effects":{"crickets":{"intensity":0,"volume":50},"birds":{"intensity":2,"volume":45},"cicadas":{"intensity":0,"volume":0},"frogs":{"intensity":1,"volume":50},"frogsAlt":{"intensity":0,"volume":0}}}' -H 'Content-Type: application/json' -X PUT http://192.168.1.8:8000/soundscape > /dev/null
+sleep 1
+echo "set frogs 1entensity 2"
+curl -s -d '{"effects":{"crickets":{"intensity":0,"volume":50},"birds":{"intensity":2,"volume":45},"cicadas":{"intensity":0,"volume":0},"frogs":{"intensity":2,"volume":50},"frogsAlt":{"intensity":0,"volume":0}}}' -H 'Content-Type: application/json' -X PUT http://192.168.1.8:8000/soundscape > /dev/null
+sleep 1
+echo "set frogs 1entensity 3"
+curl -s -d '{"effects":{"crickets":{"intensity":0,"volume":50},"birds":{"intensity":2,"volume":45},"cicadas":{"intensity":0,"volume":0},"frogs":{"intensity":3,"volume":50},"frogsAlt":{"intensity":0,"volume":0}}}' -H 'Content-Type: application/json' -X PUT http://192.168.1.8:8000/soundscape > /dev/null
+sleep 1
+echo "set frogs 1entensity 0"
+curl -s -d '{"effects":{"crickets":{"intensity":0,"volume":50},"birds":{"intensity":2,"volume":45},"cicadas":{"intensity":0,"volume":0},"frogs":{"intensity":0,"volume":50},"frogsAlt":{"intensity":0,"volume":0}}}' -H 'Content-Type: application/json' -X PUT http://192.168.1.8:8000/soundscape > /dev/null
